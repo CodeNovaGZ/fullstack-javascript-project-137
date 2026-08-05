30915e3a311953cc7f7489579084e9962591eb8c
Maicol <mdgonzalez4124@gmail.com>
2026-08-04 21:46:28 -0500

    feat: Agregar link de la pagina desplegada

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/CodeNovaGZ/fullstack-javascript-project-137/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/CodeNovaGZ/fullstack-javascript-project-137/actions)
 [![Node.js CI](https://github.com/CodeNovaGZ/fullstack-javascript-project-137/actions/workflows/node.js.yml/badge.svg)](https://github.com/CodeNovaGZ/fullstack-javascript-project-137/actions/workflows/node.js.yml)
+
+
+[text](https://agregador-mj04ae7lq-michael-codenova-projects.vercel.app/)
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+
 
 document.querySelector('#app').innerHTML = `<h1>Agregador de RSS</h1><form>
   <div class="mb-3">
@@ -11,4 +8,4 @@ document.querySelector('#app').innerHTML = `<h1>Agregador de RSS</h1><form>
   <button type="submit" class="btn btn-primary mt-2">Submit</button>
 </form>`
 
-setupCounter(document.querySelector('#counter'))
+
